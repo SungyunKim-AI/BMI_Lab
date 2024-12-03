9de7682a60d989e7299b0233b90860b61446663c
SungyunKim-AI <53232440+SungyunKim-AI@users.noreply.github.com>
2024-12-03 15:51:17 +0900

    Update gpu_stats.sh

--- monitoring/gpu_stats.sh
+++ monitoring/gpu_stats.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | while read -r line; do
+nvidia-smi --query-gpu=index,memory.used,utilization.gpu --format=csv,noheader,nounits | while read -r line; do
     # GPU 번호와 사용 중인 메모리 추출
     gpu_id=$(echo $line | awk '{print $1}' | tr -d ',')
-    gpu_mem=$(echo $line | awk '{print $2}')
-
-    echo "GPU ID: $gpu_id, Memory Used: ${gpu_mem}MiB"
+    gpu_mem=$(echo $line | awk '{print $2}' | tr -d ',')
+    gpu_util=$(echo $line | awk '{print $3}')
 
     # 해당 GPU에서 실행 중인 프로세스 정보 가져오기
     nvidia-smi --query-compute-apps=pid --format=csv,noheader --id=$gpu_id | while read -r pid; do
@@ -12,11 +11,13 @@ nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | while r
         if [ ! -z "$pid" ]; then
             container_id=$(cat /proc/$pid/cgroup | grep 'docker' | sed 's/.*-//; s/^\(.\{12\}\).*/\1/')
             if [ -z "$container_id" ]; then
-                echo "  PID: $pid, Not running inside a container"
+                echo "GPU_ID: $gpu_id, Mem_Used: ${gpu_mem}MiB, Mem_Util: ${gpu_util}%, PID: $pid, CID: none, CName: none"
             else
                 container_name=$(docker ps --filter "id=$container_id" --format "{{.Names}}")
-                echo "  PID: $pid, CID: $container_id, CName: $container_name"
+                echo "GPU ID: $gpu_id, Memory Used: ${gpu_mem}MiB, Mem_Util: ${gpu_util}%, PID: $pid, CID: $container_id, CName: $container_name"
             fi
         fi
     done
 done
+
+
